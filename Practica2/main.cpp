0412d0a502e0a9e1cebbf30ba805dd0a80d40ec8
Camilo Mazo <camilo.mazo2@udea.edu.co>
2020-10-13 00:04:37 -0500

    Problema 13 listo.

--- Practica2/main.cpp
+++ Practica2/main.cpp
@@ -1,4 +1,4 @@
-
+// Juan Camilo Mazo Castro - Practica 2 Info ll UdeA.
 // Problemas a resolver: 2,3,5,7,10,11,13,14,16,17
 
 #include <iostream>
@@ -13,6 +13,8 @@ int countDigits(int );//Indica el numero de digitos que tiene un entero.
 void makeArrayInt(int *,int ,int );//Realiza un arreglo con los digitos de un entero.
 void deleteRepeatedLyrics(char []);//Toma un arreglo char e imprime el arreglo sin los caracteres repetidos.
 void romanValue();//Pide una cadena de caracteres con un numero romano y lo transforma a un numero arabigo.
+void cinema();//Muestra los asientos del cine, luego permite reservar y cancelar puestos.
+void numberStars(float **,int ,int );//Toma la matriz e indica cuántas estrellas hay en ella.
 
 int main(){
     int selection=1;
@@ -61,6 +63,29 @@ int main(){
 
             case 10:{
                 romanValue();
+            break;
+            }
+
+            case 11:{
+                cinema();
+            break;
+            }
+
+            case 13:{
+                int filas=0,columnas=0;
+                cout<<"Introduzca el numero de filas de la matriz: "; cin>>filas;
+                cout<<"Introduzca el numbero de columnas de la matriz: "; cin>>columnas;
+                float **matrix;
+                matrix= new float *[filas];
+                for(int i=0;i<filas;i++){
+                    matrix[i]=new float[columnas];
+                }
+                for(int j=0; j<filas;j++){
+                    for(int k=0;k<columnas;k++){
+                        cout<<"Ingrese el valor para la posicion ["<<j<<"]["<<k<<"] "<<endl; cin>>*(*(matrix+j)+k);
+                    }
+                }
+                numberStars(matrix,filas,columnas);
             break;
             }
         }
@@ -289,4 +314,126 @@ void romanValue(){
     }
     if(correct==true) cout<<"El numero romano ingresado corresponde al: "<<result<<endl;
     else cout<<"El numero ingresado es invalido "<<endl;
+}
+
+void cinema(){
+    int **matrix;
+    matrix=new int *[15];
+    for(int i=0;i<15;i++){
+        matrix[i]=new int[20];
+    }
+    for(int j=0;j<15;j++){
+        for(int k=0;k<20;k++){
+            *(*(matrix+j)+k)=0;
+        }
+
+    }
+    bool finished=false;
+    while(finished==false){
+        for(int j=0;j<15;j++){
+            for(int k=0;k<20;k++){
+                if((*(*(matrix+j)+k))==0) cout<<"- ";
+                else cout<<"+ ";
+            }
+            cout<<endl;
+        }
+        char fila, option; int columna;
+        cout<<"Ingrese a para reservar un asiento "<<endl<<"Ingrese b para cancelar una reserva "<<endl; cin>>option;
+        if(option!='a' && option!='b') break;
+        cout<<"Ingrese la fila A-O "<<endl; cin>>fila;
+        cout<<"Ingrese la columna 1-20 "<<endl; cin>>columna;
+        switch (fila) {
+        case 'A':{
+            if(option=='a') *(*(matrix)+(columna-1))=1;
+            else *(*(matrix)+(columna-1))=0;
+            break;
+        }
+        case 'B':{
+            if(option=='a') *(*(matrix+1)+(columna-1))=1;
+            else *(*(matrix+1)+(columna-1))=0;
+            break;
+        }
+        case 'C':{
+            if(option=='a') *(*(matrix+2)+(columna-1))=1;
+            else *(*(matrix+2)+(columna-1))=0;
+            break;
+        }
+        case 'D':{
+            if(option=='a') *(*(matrix+3)+(columna-1))=1;
+            else *(*(matrix+3)+(columna-1))=0;
+            break;
+        }
+        case 'E':{
+            if(option=='a') *(*(matrix+4)+(columna-1))=1;
+            else *(*(matrix+4)+(columna-1))=0;
+            break;
+        }
+        case 'F':{
+            if(option=='a') *(*(matrix+5)+(columna-1))=1;
+            else *(*(matrix+5)+(columna-1))=0;
+            break;
+        }
+        case 'G':{
+            if(option=='a') *(*(matrix+6)+(columna-1))=1;
+            else *(*(matrix+6)+(columna-1))=0;
+            break;
+        }
+        case 'H':{
+            if(option=='a') *(*(matrix+7)+(columna-1))=1;
+            else *(*(matrix+7)+(columna-1))=0;
+            break;
+        }
+        case 'I':{
+            if(option=='a') *(*(matrix+8)+(columna-1))=1;
+            else *(*(matrix+8)+(columna-1))=0;
+            break;
+        }
+        case 'J':{
+            if(option=='a') *(*(matrix+9)+(columna-1))=1;
+            else *(*(matrix+9)+(columna-1))=0;
+            break;
+        }
+        case 'K':{
+            if(option=='a') *(*(matrix+10)+(columna-1))=1;
+            else *(*(matrix+10)+(columna-1))=0;
+            break;
+        }
+        case 'L':{
+            if(option=='a') *(*(matrix+11)+(columna-1))=1;
+            else *(*(matrix+11)+(columna-1))=0;
+            break;
+        }
+        case 'M':{
+            if(option=='a') *(*(matrix+12)+(columna-1))=1;
+            else *(*(matrix+12)+(columna-1))=0;
+            break;
+        }
+        case 'N':{
+            if(option=='a') *(*(matrix+13)+(columna-1))=1;
+            else *(*(matrix+13)+(columna-1))=0;
+            break;
+        }
+        case 'O':{
+            if(option=='a') *(*(matrix+14)+(columna-1))=1;
+            else *(*(matrix+14)+(columna-1))=0;
+            break;
+        }
+        default:{
+            cout<<"Los valores ingresados son invalidos "<<endl;
+        }
+        }
+    }
+    delete [] matrix;
+}
+
+void numberStars(float **p,int filas ,int columnas){
+    float result=0;
+    int count=0;
+    for(int i=1;i<(filas-1);i++){
+        for(int j=1;j<(columnas-1);j++){
+            result=(*(*(p+i)+j)+*(*(p+(i-1))+(j-1))+*(*(p+(i-1))+(j+1))+*(*(p+(i+1))+(j-1))+*(*(p+(i+1))+(j+1)))/5;
+            if(result>6) count+=1;
+        }
+    }
+    cout<<"El numero de estrellas encontradas es: "<<count<<endl;
 }
