d71c3af242e8294746ecebbd2d812e7a3c10f0e5
Camilo Mazo <camilo.mazo2@udea.edu.co>
2021-03-04 17:02:28 -0500

    Recordando

---
+++ Practica3/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string.h>
+
+using namespace std;
+
+int convertBinary(char [] );
+
+int main()
+{
+    return 0;
+}
+
+int convertBinary(char p[]){
+    while(true){
+        int i=0;
+        int numberDecimal=(int)p[i];
+
+        i+=1;
+    }
+    return 0;
+}
